3f9179fb400b8675221d9e5b2bac7068971f5363
klav28 <klav28@gmail.com>
2023-07-11 09:06:06 +0300

    3rd

--- contacts.js
+++ contacts.js
@@ -4,18 +4,33 @@ import { nanoid } from "nanoid";
 
 const contactsPath = path.resolve("db", "contacts.json");
 
+const updateStorage = (data) =>
+  fs.writeFile(contactsPath, JSON.stringify(data, null, 2));
+
 export const listContacts = async () => {
-  const data = await fs.readFile(contactsPath);
-  return JSON.parse(data);
+  const data = JSON.parse(await fs.readFile(contactsPath));
+  return data;
+  // ...твій код. Повертає масив контактів.
 };
 
-function getContactById(contactId) {
+export const getContactById = async (contactId) => {
+  const data = await listContacts();
+  const result = data.find((el) => el.id === contactId);
+  return result || null;
   // ...твій код. Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
-}
+};
 
-function removeContact(contactId) {
+export const removeContact = async (contactId) => {
+  const data = await listContacts();
+  const index = data.findIndex((el) => el.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  const [result] = data.splice(index, 1);
+  await updateStorage(data);
+  return result;
   // ...твій код. Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
-}
+};
 
 function addContact(name, email, phone) {
   // ...твій код. Повертає об'єкт доданого контакту.
@@ -23,4 +38,6 @@ function addContact(name, email, phone) {
 
 export default {
   listContacts,
+  getContactById,
+  removeContact,
 };
--- index.js
+++ index.js
@@ -1,8 +1,42 @@
 import contacts from "./contacts.js";
+import { Command } from "commander";
 
-const listContacts = async () => {
-  const result = await contacts.listContacts();
-  return result;
+const program = new Command();
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+// TODO: рефакторити
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      return console.log(await contacts.listContacts());
+      break;
+
+    case "get":
+      return console.log(await contacts.getContactById(id));
+      break;
+
+    case "remove":
+      return console.log(await contacts.removeContact(id));
+      // ... id
+      break;
+
+    case "add":
+      // ... name email phone
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
 };
 
-console.log(listContacts);
+invokeAction({ action: "remove", id: "rsKkOQUi80UsgVPCcLZZW" });
+invokeAction({ action: "list" });
