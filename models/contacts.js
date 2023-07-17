a9c7562709200041ac4330ce863ef0cf61e9b43c
klav28 <klav28@gmail.com>
2023-07-17 22:30:49 +0300

    1st complete

---
+++ models/contacts.js
@@ -0,0 +1,62 @@
+import fs from "fs/promises";
+import path from "path";
+import { nanoid } from "nanoid";
+
+const contactsPath = path.resolve("models", "contacts.json");
+
+const updateStorage = (data) =>
+  fs.writeFile(contactsPath, JSON.stringify(data, null, 2));
+
+export const listContacts = async () => {
+  // Повертає масив контактів.
+  const data = JSON.parse(await fs.readFile(contactsPath));
+  return data;
+};
+
+export const getContactById = async (contactId) => {
+  // Повертає об'єкт контакту з таким id або null, якщо контакт з таким id не знайдений.
+  const data = await listContacts();
+  const result = data.find((el) => el.id === contactId);
+  return result || null;
+};
+
+export const removeContact = async (contactId) => {
+  // Видаляє контакт та повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
+  const data = await listContacts();
+  const index = data.findIndex((el) => el.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  const [result] = data.splice(index, 1);
+  await updateStorage(data);
+  return result;
+};
+
+export const addContact = async ({ name, email, phone }) => {
+  // Додає контакт та повертає об'єкт доданого контакту.
+  const data = await listContacts();
+  const newContact = { id: nanoid(), name, email, phone };
+  data.push(newContact);
+  await updateStorage(data);
+  return newContact;
+};
+
+export const updateContact = async (contactId, { name, email, phone }) => {
+  // Оновлює контакт та повертає об'єкт оновленого контакту або null, якщо контакт не знайдено.
+  const data = await listContacts();
+  const index = data.findIndex((el) => el.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  data[index] = { contactId, name, email, phone };
+  await updateStorage(data);
+  return data[index];
+};
+
+export default {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateContact,
+};
---
+++ routes/api/contacts-router.js
@@ -0,0 +1,87 @@
+import express from "express";
+
+import Joi from "joi";
+
+import contactsService from "../../models/contacts.js";
+import { HttpError } from "../../helpers/index.js";
+
+const contactsRouter = express.Router();
+
+const contactsAddSchema = Joi.object({
+  name: Joi.string().required(),
+  phone: Joi.string().required(),
+  email: Joi.string().required().email({ minDomainSegments: 2 }),
+});
+
+contactsRouter.get("/", async (req, res, next) => {
+  try {
+    const result = await contactsService.listContacts();
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+});
+
+contactsRouter.get("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await contactsService.getContactById(id);
+    if (!result) {
+      throw HttpError(404, `Contact with id=${id} not found`);
+    }
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+});
+
+contactsRouter.post("/", async (req, res, next) => {
+  try {
+    const { error } = contactsAddSchema.validate(req.body);
+    if (error) {
+      throw HttpError(400, error.message);
+    }
+    const result = await contactsService.addContact(req.body);
+    res.status(201).json(result);
+  } catch (error) {
+    next(error);
+  }
+});
+
+contactsRouter.delete("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await contactsService.removeContact(id);
+    if (!result) {
+      throw HttpError(
+        404,
+        `Nothing to delete: Contact with id=${id} not found`
+      );
+    }
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+});
+
+contactsRouter.put("/:id", async (req, res, next) => {
+  try {
+    const { error } = contactsAddSchema.validate(req.body);
+    if (error) {
+      throw HttpError(400, error.message);
+    }
+    const { id } = req.params;
+    const result = await contactsService.updateContact(id, req.body);
+    if (!result) {
+      throw HttpError(
+        404,
+        `Nothing to update: Contact with id=${id} not found`
+      );
+    }
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+});
+
+export default contactsRouter;
