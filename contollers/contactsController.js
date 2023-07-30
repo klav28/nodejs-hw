30e4179329b89fdf3bd4916b7d85ca675e873186
klav28 <klav28@gmail.com>
2023-07-30 23:07:56 +0300

    limit, owner

--- contollers/contactsController.js
+++ contollers/contactsController.js
@@ -5,7 +5,12 @@ import { controlWrapper } from "../decorators/index.js";
 import { HttpError } from "../helpers/index.js";
 
 const getAll = async (req, res) => {
-  const result = await Contact.find({}, "-createdAt -updatedAt");
+  const { page = 1, limit = 10 } = req.query;
+  const skip = (page - 1) * limit;
+  const result = await Contact.find({}, "-createdAt -updatedAt", {
+    skip,
+    limit,
+  }).populate("owner", "email");
   res.json(result);
 };
 
@@ -19,7 +24,8 @@ const getById = async (req, res) => {
 };
 
 const add = async (req, res) => {
-  const result = await Contact.create(req.body);
+  const { _id: owner } = req.user;
+  const result = await Contact.create({ ...req.body, owner });
   res.status(201).json(result);
 };
 
--- routes/api/contacts-router.js
+++ routes/api/contacts-router.js
@@ -3,9 +3,11 @@ import express from "express";
 import contactsController from "../../contollers/contactsController.js";
 import contactsSchema from "../../schemas/contacts-joischeme.js";
 import { validateBody } from "../../decorators/index.js";
-import { isEmptyBody, isValidId } from "../../middleware/index.js";
+import { authenticate, isEmptyBody, isValidId } from "../../middleware/index.js";
 
 const contactsRouter = express.Router();
+
+contactsRouter.use(authenticate);
 
 contactsRouter.get("/", contactsController.getAll);
 
